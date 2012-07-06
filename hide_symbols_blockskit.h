5735477d7ab7da8eb870b2bd5b7102d6035284fd
Nathaniel Griswold <nategriswold@gmail.com>
2012-07-06 16:04:19 -0700

    Make symbol hiding generic

--- hide_symbols_blockskit.h
+++ hide_symbols_blockskit.h
@@ -1,14 +1,22 @@
-#define BKDelegate  SZBKDelegate
-#define BlocksKit  SZBlocksKit
-#define BKMailComposeViewControllerDelegate  SZBKMailComposeViewControllerDelegate
-#define BKMessageComposeViewControllerDelegate  SZBKMessageComposeViewControllerDelegate
-#define BKCacheDelegate  SZBKCacheDelegate
-#define BKInvocationGrabber SZBKInvocationGrabber
-#define AssociatedObjects SZAssociatedObjects
-#define BlockObservation SZBlockObservation
-#define BKObserver SZBKObserver
-#define BlocksKitPrivate SZBlocksKitPrivate
-#define BKURLConnectionDelegate  SZBKURLConnectionDelegate
-#define BKControlWrapper SZBKControlWrapper
-#define BlocksKitInternal SZBlocksKitInternal
-#define BKWebViewDelegate  SZBKWebViewDelegate
+#ifndef BKPREFIX
+#define BKPREFIX BK_HIDDEN_
+#endif
+
+#define BKPASTER(x,y) x ## y
+#define BKEVALUATOR(x,y)  BKPASTER(x,y)
+#define BKP(sym) BKEVALUATOR(BKPREFIX, sym)
+
+#define BKDelegate BKP(BKDelegate)
+#define BlocksKit  BKP(BlocksKit)
+#define BKMailComposeViewControllerDelegate  BKP(BKMailComposeViewControllerDelegate)
+#define BKMessageComposeViewControllerDelegate  BKP(BKMessageComposeViewControllerDelegate)
+#define BKCacheDelegate  BKP(BKCacheDelegate)
+#define BKInvocationGrabber BKP(BKInvocationGrabber)
+#define AssociatedObjects BKP(AssociatedObjects)
+#define BlockObservation BKP(BlockObservation)
+#define BKObserver BKP(BKObserver)
+#define BlocksKitPrivate BKP(BlocksKitPrivate)
+#define BKURLConnectionDelegate  BKP(BKURLConnectionDelegate)
+#define BKControlWrapper BKP(BKControlWrapper)
+#define BlocksKitInternal BKP(BlocksKitInternal)
+#define BKWebViewDelegate  BKP(BKWebViewDelegate)
