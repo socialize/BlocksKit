3a1ffd886aeaeaa55b3b983212fb1beca05ab60f
David Jedeikin <djedeikin@sharethis.com>
2014-10-07 11:00:29 -0700

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit 8f1477f999be096a84a8c94e21b4e006a3c1a68a.

--- BlocksKit/BlocksKit+QuickLook.h
+++ BlocksKit/BlocksKit+QuickLook.h
@@ -1,10 +1,9 @@
 //
-//  BlocksKit+UIKit
+//  BlocksKit+QuickLook
 //
 //  The Objective-C block utilities you always wish you had.
 //
-//  Copyright (c) 2011-2012, 2013-2014 Zachary Waldowski
-//  Copyright (c) 2012-2013 Pandamonia LLC
+//  Copyright (c) 201 Boolean Candy Pty Ltd
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -25,14 +24,4 @@
 //  THE SOFTWARE.
 //
 
-#import <BlocksKit/UIActionSheet+BlocksKit.h>
-#import <BlocksKit/UIAlertView+BlocksKit.h>
-#import <BlocksKit/UIBarButtonItem+BlocksKit.h>
-#import <BlocksKit/UIControl+BlocksKit.h>
-#import <BlocksKit/UIGestureRecognizer+BlocksKit.h>
-#import <BlocksKit/UIPopoverController+BlocksKit.h>
-#import <BlocksKit/UITextField+BlocksKit.h>
-#import <BlocksKit/UIView+BlocksKit.h>
-#import <BlocksKit/UIWebView+BlocksKit.h>
-#import <BlocksKit/UITextField+BlocksKit.h>
-#import <BlocksKit/UIImagePickerController+BlocksKit.h>
+#import <BlocksKit/QLPreviewController+BlocksKit.h>
